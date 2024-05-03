8fea2bf7bfbd201f3a5cad3d5d41b382b81a66d0
Nathan Do <5110105+nathando91@users.noreply.github.com>
2024-05-03 11:49:52 +0700

    Update mybash.sh

--- mybash.sh
+++ mybash.sh
@@ -5,3 +5,6 @@ done
 for i in {1..16}; do
     google-chrome --profile-directory="Profile $i" "https://chromewebstore.google.com/detail/proxy-switchyomega/padekgcemlokbadohgkifijomclgjgif" &
 done
+for i in {1..16}; do
+    google-chrome --profile-directory="Profile $i" "https://chainlist.org/chain/81457" &
+done
