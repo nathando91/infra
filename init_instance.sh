9445d71c0abdb8223948b3ca4b13a104cbe3df4d
Nathan <doduytu@gmail.com>
2024-09-27 22:10:51 +0700

    aa

--- init_instance.sh
+++ init_instance.sh
@@ -3,11 +3,8 @@
 # Cập nhật hệ thống
 sudo apt update && sudo apt upgrade -y
 echo "openssh-server openssh-server/sshd_config select true" | sudo debconf-set-selections
-sudo apt-get install -y openssh-server
-sudo apt install unattended-upgrades
-sudo dpkg-reconfigure --priority=low unattended-upgrades
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y openssh-server
 sudo apt upgrade -y
-
 # Cài đặt các tiện ích cần thiết
 sudo apt install -y curl wget git zsh
 
